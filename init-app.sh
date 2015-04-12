404e4d43c6d559d4ae54b731da9341b6a24ff033
Matt Edlefsen <matt@xforty.com>
2015-04-12 14:17:11 -0700

    Fix bundle install

--- init-app.sh
+++ init-app.sh
@@ -11,9 +11,12 @@ fi
 mkdir log
 chown app log
 
+mkdir .bundle vendor
+chown app .bundle vendor
 setuser app bash -lc 'bundle install --deployment'
+chown root -R .bundle vendor
 
-TMP="$(mktemp -d)"
+TMP="$(setuser app mktemp -d)"
 setuser app bash -lc "foreman export -d /srv/app -u app -p 80 runit '$TMP'"
 mv "$TMP"/* /etc/service
 rmdir "$TMP"
